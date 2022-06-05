b468973557b11d036246a126f63ae03acf9cfb72
avan1235 <macieekprocyk@gmail.com>
2022-06-05 20:57:24 +0000

    clean code and restructure libs

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import qualified MyLib (someFunc)
-
-import Web.Scotty
-import Network.Wai.Middleware.RequestLogger
-import qualified Data.Text.Lazy as TL
-
-import Data.Monoid (mconcat)
+import qualified ServerLib (server)
 
 main :: IO ()
-main = scotty 3000 $ do
-  middleware logStdoutDev
-
-  get "/:test" $ do
-    test <- param "test"
-    text $ mconcat ["You're at ", test, " page!"]
-
-  post "/upload/image" $ do
-    formContent <- (param "image[data]") `rescue` (\msg -> return msg)
-    text $ "received: " `TL.append` formContent
+main = ServerLib.server
