25c8577c7381d37117c2f37e7b4e1c4792cf9f2b
avan1235 <macieekprocyk@gmail.com>
2022-06-05 20:27:55 +0000

    add sample webserver

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified MyLib (someFunc)
 
+import Web.Scotty
+import Network.Wai.Middleware.RequestLogger
+import qualified Data.Text.Lazy as TL
+
+import Data.Monoid (mconcat)
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = scotty 3000 $ do
+  middleware logStdoutDev
+
+  get "/:test" $ do
+    test <- param "test"
+    text $ mconcat ["You're at ", test, " page!"]
+
+  post "/upload/image" $ do
+    formContent <- (param "image[data]") `rescue` (\msg -> return msg)
+    text $ "received: " `TL.append` formContent
