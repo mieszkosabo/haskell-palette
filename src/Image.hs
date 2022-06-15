466053d9f9ebce55ac7e69e938204889d37e4610
Mieszko Sabo <51922326+mieszkosabo@users.noreply.github.com>
2022-06-15 12:38:40 +0200

    refactor (#4)
    Co-authored-by: Mieszko Sabo <mieszko.mac@gmail.com>

--- src/Image.hs
+++ src/Image.hs
@@ -1,8 +1,8 @@
 module Image (loadImage, FileName, ErrorMessage, Image) where
-import qualified Codec.Picture as P
 
-type FileName = String
-type ErrorMessage = String
+import qualified Data.Either.Combinators as E
+import qualified Codec.Picture as P
+import CommonTypes
 
 -- alias for JuicyImage type for brevity
 type Image = P.Image P.PixelRGBA8
@@ -10,8 +10,4 @@ type Image = P.Image P.PixelRGBA8
 loadImage :: FileName -> IO (Either ErrorMessage (P.Image P.PixelRGBA8))
 loadImage fileName = do
     io <- P.readImage fileName
-
-    case io of
-        Left err -> return $ Left "Error: Unable to load the image:"
-        Right dynamicImage -> return $ Right $ P.convertRGBA8 dynamicImage
-
+    return $ E.mapBoth ((++) "Error: Unable to load the image: ") P.convertRGBA8 io
