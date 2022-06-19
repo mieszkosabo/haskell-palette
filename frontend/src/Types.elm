b340e1508f3ebd7926e10052cb0749e24896257f
Mieszko Sabo <51922326+mieszkosabo@users.noreply.github.com>
2022-06-19 23:32:37 +0200

    fancy button and loading state (#20)
    Co-authored-by: Mieszko Sabo <mieszko.mac@gmail.com>

--- frontend/src/Types.elm
+++ frontend/src/Types.elm
@@ -6,10 +6,12 @@ import File exposing (File)
 type alias Color = String
 type alias ImageString = String
 type Algorithm = MedianCut | Kmeans | KmeansPP
+type LocalState = Initial | Loading | ShowingPalette | Error
 
-
-
+-- TODO: refactor into union of possible states (eg. we know that image
+-- is Nothing in Error localState, and Just image in ShowingPalette, etc...)
 type alias State = {
+    state: LocalState,
     algorithm: Maybe Algorithm,
     image: Maybe ImageString,
     colors : List Color,
